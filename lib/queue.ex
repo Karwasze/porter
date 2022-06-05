42b726c0cc89349e26e8b068325a8dcc20160b5a
Karol Kędzierski <karolkedzierski@live.com>
2022-06-05 19:39:21 +0200

    Removed unnecessary functions from Queue module

--- lib/queue.ex
+++ lib/queue.ex
@@ -9,10 +9,6 @@ defmodule Queue do
     Agent.get(__MODULE__, fn list -> List.first(list, []) end)
   end
 
-  def get2 do
-    Agent.get(__MODULE__, fn [head | _] -> head end)
-  end
-
   def get_all do
     Agent.get(__MODULE__, fn list -> list end)
   end
@@ -21,15 +17,7 @@ defmodule Queue do
     Agent.update(__MODULE__, fn list -> list ++ [url] end)
   end
 
-  def shift(url) do
-    Agent.update(__MODULE__, fn list -> [url | list] end)
-  end
-
   def remove() do
     Agent.update(__MODULE__, fn list -> List.delete_at(list, 0) end)
-  end
-
-  def remove2() do
-    Agent.update(__MODULE__, fn [head | _] -> head end)
   end
 end
