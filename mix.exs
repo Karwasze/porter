b09be4f2711150bf7ac895ad8c5f20b52fd2bfcb
Karol Kędzierski <karolkedzierski@live.com>
2022-07-04 18:58:51 +0200

    Switched to a forked version of "alchemy" package

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Porter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:alchemy, "~> 0.7.0", hex: :discord_alchemy}
+      {:alchemy, git: "https://github.com/Karwasze/alchemy.git"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
