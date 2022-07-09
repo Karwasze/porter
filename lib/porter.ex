0742f155e2c753fee5e92773150780f73b9b76a6
Karol Kędzierski <karolkedzierski@live.com>
2022-07-09 20:50:03 +0200

    Removed unnecessary stop

--- lib/porter.ex
+++ lib/porter.ex
@@ -192,7 +192,6 @@ defmodule AudioPlayerConsumer do
 
       "!leave" ->
         init_if_new_guild(msg.guild_id)
-        StopReason.set_stopped(msg.guild_id)
         Voice.stop(msg.guild_id)
         Queue.remove_all(msg.guild_id)
         Lock.unlock(msg.guild_id)
