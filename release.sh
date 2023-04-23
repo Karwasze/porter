4b325a8f0ea521302301f864a7f03575b84b8e81
Karol Kędzierski <karolkedzierski@live.com>
2023-04-23 13:28:35 +0200

    Updated release script

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 /usr/bin/git pull
 /usr/bin/docker build . --no-cache --build-arg DISCORD_TOKEN=$1 -t porter:latest
 /usr/bin/docker stop porter
-/usr/bin/docker rm -f porter
-/usr/bin/docker run --name porter -d porter:latest
+/usr/bin/docker run --rm --name porter -d porter:latest
+/usr/bin/docker image prune -f
